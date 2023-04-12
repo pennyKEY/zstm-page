761bd1833ec113798020a445c6361731f0c29314
Pawel Kaczmarczyk <pawelondko666666@gmail.com>
2023-04-13 00:38:49 +0200

    add font inter to project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
 import Head from "next/head";
-import { Inter } from "next/font/google";
 
 import Header from "@/components/pages/start/Header";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
