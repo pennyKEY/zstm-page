d14a1e24be1ce4456abda7e501f3319616683d98
Pawel Kaczmarczyk <pawelondko666666@gmail.com>
2023-04-24 12:34:23 +0200

    change slider properties and view

--- components/pages/start/Posts.tsx
+++ components/pages/start/Posts.tsx
@@ -23,48 +23,87 @@ export default function Posts({ postsDivRef, activeIndexPost }: Props) {
       title: "Dni otwarte",
       img: post3.src,
     },
+    {
+      title: "Uroczyste Rozpoczęcie Roku Szkolnego 2022/2023",
+      img: post1.src,
+    },
+    {
+      title: "PRZETARG NIEOGRANICZONY NA DZIERŻAWĘ SKLEPIKU SZKOLNEGO",
+      img: post2.src,
+    },
+    {
+      title: "ROZPOCZĘCIE ROKU SZKOLNEGO",
+      img: post3.src,
+    },
   ];
 
   function translateFunction() {
     if (activeIndexPost === 0) {
       return `translateX(0%)`;
     } else if (activeIndexPost > 0) {
-      return `translateX(-${activeIndexPost * 33.33}%)`;
+      return `translateX(-${activeIndexPost * (100 / 6)}%)`;
     }
   }
 
   return (
-    <div
-      className="posts"
-      ref={postsDivRef}
-      style={{
-        transform: translateFunction(),
-      }}
-    >
-      <article className="post">
+    <div className="posts" ref={postsDivRef}>
+      <div className="posts__container">
         <div
-          className="post__bcg"
-          style={{ backgroundImage: `url('${posts[0].img}')` }}
+          className="posts__wrapper"
+          style={{
+            transform: translateFunction(),
+          }}
         >
-          <h6 className="post__title">{posts[0].title}</h6>
+          <article className="post">
+            <div
+              className="post__bcg"
+              style={{ backgroundImage: `url('${posts[0].img}')` }}
+            >
+              <h6 className="post__title">{posts[0].title}</h6>
+            </div>
+          </article>
+          <article className="post">
+            <div
+              className="post__bcg"
+              style={{ backgroundImage: `url('${posts[1].img}')` }}
+            >
+              <h6 className="post__title">{posts[1].title}</h6>
+            </div>
+          </article>
+          <article className="post">
+            <div
+              className="post__bcg"
+              style={{ backgroundImage: `url('${posts[2].img}')` }}
+            >
+              <h6 className="post__title">{posts[2].title}</h6>
+            </div>
+          </article>
+          <article className="post">
+            <div
+              className="post__bcg"
+              style={{ backgroundImage: `url('${posts[3].img}')` }}
+            >
+              <h6 className="post__title">{posts[3].title}</h6>
+            </div>
+          </article>
+          <article className="post">
+            <div
+              className="post__bcg"
+              style={{ backgroundImage: `url('${posts[4].img}')` }}
+            >
+              <h6 className="post__title">{posts[4].title}</h6>
+            </div>
+          </article>
+          <article className="post">
+            <div
+              className="post__bcg"
+              style={{ backgroundImage: `url('${posts[5].img}')` }}
+            >
+              <h6 className="post__title">{posts[5].title}</h6>
+            </div>
+          </article>
         </div>
-      </article>
-      <article className="post">
-        <div
-          className="post__bcg"
-          style={{ backgroundImage: `url('${posts[1].img}')` }}
-        >
-          <h6 className="post__title">{posts[1].title}</h6>
-        </div>
-      </article>
-      <article className="post">
-        <div
-          className="post__bcg"
-          style={{ backgroundImage: `url('${posts[2].img}')` }}
-        >
-          <h6 className="post__title">{posts[2].title}</h6>
-        </div>
-      </article>
+      </div>
     </div>
   );
 }
--- hooks/useNavigationPosts.ts
+++ hooks/useNavigationPosts.ts
@@ -4,9 +4,17 @@ export default function useNavigationPosts() {
   const [activeIndexPost, setActiveIndexPost] = useState(0);
 
   const changeActiveIndex = (grow: boolean) => {
+    const widthWindow = window.innerWidth;
+
+    let stop: number = 5;
+
+    if (widthWindow >= 1024) {
+      stop = 3;
+    }
+
     if (grow === false && activeIndexPost === 0) {
       return null;
-    } else if (grow === true && activeIndexPost === 2) {
+    } else if (grow === true && activeIndexPost === stop) {
       return null;
     }
 
